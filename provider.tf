ac551a8c9b05790b1cf2ab957066b189e5f4d68c
biprodatta <109247679+biprodatta@users.noreply.github.com>
2022-07-16 16:20:14 -0400

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -10,4 +10,5 @@ provider "aws" {
   shared_config_files      = ["~/.aws/config"]
   shared_credentials_files = ["~/.aws/credentials"]
   profile                  = "default"
+  region     = var.region
 }
