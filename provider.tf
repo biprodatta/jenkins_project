46b548c278825fd32d97994faa33ed7f33d4632b
biprodatta <109247679+biprodatta@users.noreply.github.com>
2022-07-16 16:21:32 -0400

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -7,8 +7,8 @@ terraform {
   }
 }
 provider "aws" {
-  shared_config_files      = ["~/.aws/config"]
-  shared_credentials_files = ["~/.aws/credentials"]
+  shared_config_file      = ["~/.aws/config"]
+  shared_credentials_file = ["~/.aws/credentials"]
   profile                  = "default"
   region     = var.region
 }
