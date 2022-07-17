c361b733c4c08a7e74442f319e9915b45f11602e
biprodatta <109247679+biprodatta@users.noreply.github.com>
2022-07-17 01:20:23 -0400

    Update main.tf

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -12,6 +12,7 @@ resource "aws_instance" "web" {
   ami           = data.aws_ami.amazon-2.id
   instance_type = "t3.micro"
   key_name = "bipro_aws_key"
+  vpc_security_group_ids = "sg-00973a2eb6c626748"
 
   user_data = <<-EOL
   #!/bin/bash -xe
