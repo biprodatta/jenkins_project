0a0cc4cb4c024772dbc3a3b5c826ef5b2cf42363
biprodatta <109247679+biprodatta@users.noreply.github.com>
2022-07-17 00:35:36 -0400

    Update main.tf

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,15 +1,5 @@
-data "aws_ami" "amazon-2" {
-  most_recent = true
-
-  filter {
-    name = "name"
-    values = ["amzn2-ami-hvm-*-x86_64-ebs"]
-  }
-  owners = ["amazon"]
-}
-
 resource "aws_instance" "web" {
-  ami           = data.aws_ami.amazon-2.id
+  ami           = ami-02d1e544b84bf7502
   instance_type = "t3.micro"
 
   user_data = <<-EOL
