5fc3af69059493e515e082e538e87aebf0f810a9
biprodatta <109247679+biprodatta@users.noreply.github.com>
2022-07-16 22:14:26 -0400

    Update main.tf

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,21 +1,37 @@
-data "aws_caller_identity" "current" {}
+data "aws_ami" "amazon-linux-2" {
+ most_recent = true
 
-data "aws_ami" "ec2_instance" {
-  most_recent = true
 
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
+ filter {
+   name   = "owner-alias"
+   values = ["amazon"]
+ }
 
-  owners = ["${data.aws_caller_identity.current.account_id}"]
+
+ filter {
+   name   = "name"
+   values = ["amzn2-ami-hvm*"]
+ }
 }
 
 resource "aws_instance" "web" {
-  ami           = "${data.aws_ami.ec2_instance.id}"
-  instance_type = "t2.micro"
+  ami           = data.aws_ami.amazon-linux-2.id
+  instance_type = "t3.micro"
+
+  user_data = <<-EOL
+  #!/bin/bash -xe
+  
+  curl -fsSL https://get.docker.com | sh;
+  yum search docker
+  yum info docker
+  yum install docker
+  sudo usermod -aG docker $USER
+  chmod 666 /var/run/docker.sock
+  systemctl start docker
+  docker version
+  EOL
 
   tags = {
-    Name = "EC2 Demo Instance"
+    Name = "terraform_project"
   }
 }
